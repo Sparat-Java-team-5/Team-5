efaae2aa11143d3ab67219913f5150053e56b33d
김현수 <ks12467@naver.com>
2024-08-05 18:18:14 +0900

    [Update] 점수 등록

--- src/camp/CampManagementApplication.java
+++ src/camp/CampManagementApplication.java
@@ -7,8 +7,6 @@ import camp.model.Subject;
 import java.lang.reflect.Array;
 import java.util.*;
 import java.util.stream.Stream;
-import java.util.Map;
-import java.util.Map.Entry;
 
 
 /**
@@ -26,7 +24,7 @@ public class CampManagementApplication {
     private static List<Subject> subjectStore;
     private static List<Score> scoreStore;
     private static Map<String, ArrayList> subjectTakenStore;
-
+    private static Map<String, Map<String, List<Integer>>> scoreMap = new HashMap<>();
 
     // 과목 타입
     private static String SUBJECT_TYPE_MANDATORY = "MANDATORY"; // 필수 과목 타입
@@ -42,6 +40,7 @@ public class CampManagementApplication {
 
     // 스캐너
     private static Scanner sc = new Scanner(System.in);
+
 
     public static void main(String[] args) {
         setInitData();
@@ -208,11 +207,11 @@ public class CampManagementApplication {
                 if (subject.getSubjectId().equals(subjectId) && subject.getSubjectType().equals(SUBJECT_TYPE_MANDATORY)) {
                     if (!subjectTaken.contains(subjectId)) { //중복 등록 방지
                         subjectTaken.add(subjectId); //필수 과목 리스트에 추가
-                        System.out.println(subject.getSubjectName() + " 등록이 완료되었습니다.");
+                        System.out.println("["+subject.getSubjectName() + " 등록이 완료되었습니다.]");
                         System.out.println(subjectTaken);
                         isContain = true;
                     } else {
-                        System.out.println(subject.getSubjectName() + " 과목은 이미 등록되었습니다.");
+                        System.out.println("["+subject.getSubjectName() + " 과목은 이미 등록되었습니다.]");
                     }
                 }
             }
@@ -221,7 +220,6 @@ public class CampManagementApplication {
             }
 
             if (3 <= subjectTaken.size() && subjectTaken.size() < 5) {
-
                 while (true) {
                     System.out.println("[==================================]");
                     System.out.println("===[필수 과목은 3개 이상만 수강하시면 됩니다]===.\n" +
@@ -235,6 +233,7 @@ public class CampManagementApplication {
                         System.out.println("===[현재 등록된 필수 과목 리스트입니다. " + subjectTaken.size() + "과목이 등록되었습니다]===");
                         System.out.println("====[신청할 필수 과목 번호를 입력해주세요.]====");
                         System.out.println(subjectTaken);
+                        break;
                     } else {
                         System.out.println("[잘못입력하였습니다.]");
                     }
@@ -271,11 +270,11 @@ public class CampManagementApplication {
                 if (subject.getSubjectId().equals(subjectId) && subject.getSubjectType().equals(SUBJECT_TYPE_CHOICE)) {
                     if (!subjectTaken.contains(subjectId)) { //중복 등록 방지
                         subjectTaken.add(subjectId); //필수 과목 리스트에 추가
-                        System.out.println(subject.getSubjectName() + " 등록이 완료되었습니다.");
+                        System.out.println("["+subject.getSubjectName() + " 등록이 완료되었습니다.]");
                         System.out.println(subjectTaken);
                         isContain = true;
                     } else {
-                        System.out.println(subject.getSubjectName() + " 과목은 이미 등록되었습니다.");
+                        System.out.println("["+subject.getSubjectName() + " 과목은 이미 등록되었습니다.]");
                     }
                     if (isContain == false) {
                         System.out.println("[잘못된 입력입니다.]");
@@ -284,7 +283,6 @@ public class CampManagementApplication {
             }
 
             if (5 <= subjectTaken.size() && subjectTaken.size() < 9) {
-
 
                 while (true) {
                     System.out.println("==================================");
@@ -298,6 +296,7 @@ public class CampManagementApplication {
                     } else if (exit.equals("no")) {
                         System.out.println("===[선택 과목으로 이동합니다.]===");
                         System.out.println(subjectTaken);
+                        break;
                     } else {
                         System.out.println("===[잘못입력하였습니다.]===");
                     }
@@ -322,8 +321,6 @@ public class CampManagementApplication {
         System.out.println("****** 수강생 등록 및 과목 등록 성공! *****\n");
 
     }
-
-
     // 수강생 목록 조회
     private static void inquireStudent() {
         System.out.println("\n수강생 목록을 조회합니다...");
@@ -366,117 +363,63 @@ public class CampManagementApplication {
         }
     }
 
-    //--------------------------------
-//    private static String getStudentId() {
-//        System.out.print("\n관리할 수강생의 번호를 입력하시오...");
-//        return sc.next();
-//    }
-//
-//    // 수강생의 과목별 시험 회차 및 점수 등록
-//    private static void createScore() {
-//        String studentId = getStudentId(); // 관리할 수강생 고유 번호
-//        System.out.println("시험 점수를 등록합니다...");
-//        // 기능 구현
-//        System.out.println("\n점수 등록 성공!");
-//    }
-//---------------------------------------------
     private static String getStudentId() {
-        System.out.print("\n관리할 수강생의 번호를 입력하시오...");
+        System.out.print("\n==[관리할 수강생의 번호를 입력하시오]===");
         String studentId = sc.next();
         if (studentStore.isEmpty()) {
-            return ("등록된 수강생이 없습니다.");
+            return ("===[등록된 수강생이 없습니다.]===");
         }
         for (Student student : studentStore) {
             if (student.getStudentId().equals(studentId)) {
                 return studentId;
             }
         }
-        System.out.println("해당 ID를 가지고 있는 수강생이 없습니다.");
-        return null;
-    }
-
-    private static String getSubjectId(String studentId) {
-        System.out.println(studentId + "이 수강한 과목");
-        ArrayList<String> subjects = subjectTakenStore.get(studentId);
-        System.out.println(subjects);
-        System.out.println("수강한 과목 중 점수를 등록할 과목명을 입력해 주세요 : ");
-        String subjectName = sc.next();
-        for (Subject subject : subjectStore) {
-            if (subject.getSubjectName().equals(subjectName)) {
-                //과목 고유 ID리턴
-                return subject.getSubjectId();
-            }
-        }
-        System.out.println("해당 과목을 수강하지 않았습니다.");
+        System.out.println("===[해당 ID를 가지고 있는 수강생이 없습니다.]===");
         return null;
     }
 
     private static void createScore() {
+
         String studentId = getStudentId(); // 관리할 수강생 고유 번호
         if (studentId == null) {
             return; // 수강생이 없거나 잘못된 ID인 경우 메서드 종료
         }
 
-        System.out.println("======== [[" + studentId +
-                "]] 시험 점수를 등록합니다 =========");
+        System.out.println("===[시험 점수를 등록합니다]===");
+        // 수강생의 과목 정보를 가져옴
+        ArrayList<String> subjects = subjectTakenStore.get(studentId);
 
-        // 수강생의 과목 정보를 가져옴 & 점수 등록할 과목 ID 가져옴
-        String subjectId = getSubjectId(studentId);
-        System.out.println(subjectId);
-        Score scoreObject = new Score(sequence(INDEX_TYPE_SCORE), studentId, subjectId);
-        for (int j=0;j<1;j++) {
-
-            System.out.println(scoreObject.getScoreId());
-            System.out.println("=========점수를 등록합니다=========");
-            Map<Integer, Integer> roundScore = new HashMap<>();
-            for (int i = 0; i < 10; i++) {
-                System.out.println(i + 1 + " 회차 점수를 입력해 주세요 : ");
-                int scoreInput = sc.nextInt();
-                roundScore.put(i + 1, scoreInput);
+        boolean againSc = true;
+        while (againSc) {
+            for (int i = 0; i < subjects.size(); i++) {
+                System.out.println("[" + (i + 1) + "]" + " : " + subjects.get(i));
             }
-            scoreObject.setRoundScore(roundScore);
+            System.out.println("===[점수를 등록할 과목 번호를 입력해주세요]===");
+            String subjectId = sc.next();
 
+            String selectedSubject = subjectId;
+            System.out.println("[선택한 과목입니다] : " + selectedSubject);
 
-            scoreStore.add(scoreObject);
-            System.out.println(scoreObject.getRoundScore().keySet());
-            System.out.println(scoreObject.getRoundScore().values());
+            Map<String, List<Integer>> studentScores = scoreMap.getOrDefault(subjectId, new HashMap<>());
 
+            List<Integer> scores = studentScores.getOrDefault(selectedSubject, new ArrayList<>());
 
-            for(Score score : scoreStore){
-                System.out.println(score.getScoreId());
-                System.out.println(score.getStudentId());
-                System.out.println(score.getSubjectId());
-                System.out.println(score.getRoundScore());
+            int round = scores.size() + 1;
+            System.out.println("["+round + "회차 점수를 입력해주세요.]");
+
+            int score = sc.nextInt();
+            scores.add(score); // 점수를 리스트에 추가
+
+            studentScores.put(selectedSubject, scores);
+            scoreMap.put(subjectId, studentScores);
+
+            System.out.println("\n===[점수 등록 성공!]===");
+            System.out.println("===[다른 과목 점수를 등록하시겠습니까? (yes / no)]===");
+            String againScore = sc.next();
+            if (!againScore.equals("yes")) {
+                againSc = false;
             }
-//            if (!score.getSubjectId().equals(subjectId)) {
-//                Score scoreObject = new Score(sequence(INDEX_TYPE_SCORE), studentId, subjectId);
-//                System.out.println("=========점수를 등록합니다=========");
-//                Map<Integer, Integer> roundScore = new HashMap<>();
-//                for (int i = 0; i < 10; i++) {
-//                    System.out.println(i + 1 + " 회차 점수를 입력해 주세요 : ");
-//                    int scoreInput = sc.nextInt();
-//                    roundScore.put(i + 1, scoreInput);
-//                }
-//                score.setRoundScore(roundScore);
-//
-//                scoreStore.add(scoreObject);
-//                System.out.println(scoreObject.getRoundScore().keySet());
-//                System.out.println(scoreObject.getRoundScore().values());
-//            }
-//            if (score.getSubjectId().equals(subjectId)) {
-//                System.out.println("이미 해당 과목 점수가 존재합니다.");
-//                if (score.getRoundScore().isEmpty()) {
-//                    System.out.println("점수 비어있음 : " + score.getRoundScore());
-//                } else {
-//                    System.out.println("점수 있음 : " + score.getRoundScore());
-//                }
-//                break;
-//            }
         }
-
-
-
-        System.out.println("\n점수 등록 성공!");
     }
 
 
@@ -499,7 +442,6 @@ public class CampManagementApplication {
 
             System.out.println("\n점수 수정 성공!");
         }
-
 
 
         //
