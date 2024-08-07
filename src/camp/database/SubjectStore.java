c751065ba62647fc7c02b4d3dfc5a1325059b823
Minkyeongweb <jangminkyeong99@gmail.com>
2024-08-07 16:30:13 +0900

    [UPDATE]SunjectStore & SubjectService

--- src/camp/database/SubjectStore.java
+++ src/camp/database/SubjectStore.java
@@ -2,14 +2,32 @@ package camp.database;
 
 import camp.model.Subject;
 
+import java.util.ArrayList;
 import java.util.List;
 
 //과목 데이터 저장소
 public class SubjectStore {
     //과목 정보를 저장하는 리스트 자료구조
-    private static List<Subject> subjectStore;
+    private static List<Subject> subjectStore = new ArrayList<>();
 
     //getter
+    public static List<Subject> getSubjectStore() {
+        return subjectStore;
+    }
 
     //setter
+    public static void setSubjectStore(List<Subject> subjectStore) {
+        SubjectStore.subjectStore = subjectStore;
+    }
+
+    //과목 추가 메서드
+    public static void addSubject(Subject subject) {
+        subjectStore.add(subject);
+    }
+
+    //과목 삭제 메서드
+    public static void removeSubject(Subject subject) {
+        subjectStore.remove(subject);
+    }
 }
+
